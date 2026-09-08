02462470913c03c586763d3af3e38f623b9f119c
Sahil Chaurasia <139702894+Wahsahil@users.noreply.github.com>
2026-09-09 00:40:46 +0530

    Time: 984 ms (11.37%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,13 @@
+SELECT product_id,
+       year AS first_year,
+       quantity,
+       price
+FROM (
+    SELECT *,
+           RANK() OVER (
+               PARTITION BY product_id
+               ORDER BY year
+           ) AS rn
+    FROM Sales
+) x
+WHERE rn = 1;
