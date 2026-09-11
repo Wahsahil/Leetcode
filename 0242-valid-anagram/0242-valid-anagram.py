05864b0c2d4966883aa936d8543a5333b41852e9
Sahil Chaurasia <139702894+Wahsahil@users.noreply.github.com>
2026-09-11 12:47:09 +0530

    Time: 12 ms (91.25%), Space: 12.5 MB (86.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        mp = {}
+        for ch in s:
+            mp[ch] = mp.get(ch, 0) + 1
+        for ch in t:
+            if ch not in mp:
+                return False
+            mp[ch] -= 1
+        for ch in mp:
+            if mp[ch] != 0:
+                return False
+        return True
