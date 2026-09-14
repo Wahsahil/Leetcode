4f196b55cca1ceb18baebecd2ef86f02625fa1c9
Sahil Chaurasia <139702894+Wahsahil@users.noreply.github.com>
2026-09-14 11:36:44 +0530

    Time: 0 ms (100%), Space: 20 MB (20.34%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,9 +1,4 @@
 class Solution(object):
     def reverseString(self, s):
-        l = 0
-        h = len(s)-1
-        while l<h:
-            s[l],s[h] = s[h],s[l]
-            l+=1
-            h-=1
+        s[:] = s[::-1]
         
