1a67586accb74973670d4fe3745c4fb4cfa4ce76
Sahil Chaurasia <139702894+Wahsahil@users.noreply.github.com>
2026-09-18 19:58:11 +0530

    Time: 4 ms (47.22%), Space: 12.6 MB (63.75%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -1,11 +1,18 @@
-
 class Solution(object):
     def duplicateZeros(self, arr):
-        ans = []
-        for i in range(len(arr)):
-            if arr[i] == 0:
-                ans.append(0)
-                ans.append(0)
-            else:
-                ans.append(arr[i])
-        arr[:] = ans[:len(arr)]
+        n = len(arr)
+        cz = 0
+        for i in arr:
+            if i ==0:
+                cz+=1
+        i = n-1
+        j = n+cz-1
+        while i<j:
+            if j<n:
+                arr[j]=arr[i]
+            if arr[i]==0:
+                j-=1
+                if j<n:
+                    arr[j]=0
+            i -= 1
+            j -= 1
