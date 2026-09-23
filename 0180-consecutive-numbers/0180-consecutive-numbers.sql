b8c49d1b6992b9cc24574979ac31cc4eada16657
Sahil Chaurasia <139702894+Wahsahil@users.noreply.github.com>
2026-09-23 15:07:52 +0530

    Time: 608 ms (60.77%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,10 +1,9 @@
-# Write your MySQL query statement below
-SELECT DISTINCT num AS ConsecutiveNums
-FROM (
-    SELECT num,
-           LAG(num, 1) OVER (ORDER BY id) AS prev1,
-           LAG(num, 2) OVER (ORDER BY id) AS prev2
-    FROM Logs
-) x
-WHERE num = prev1
-  AND num = prev2;
+with x as (
+    select *,
+    lag(num,1) over(order by id) as pre1,
+    lag(num,2) over(order by id) as pre2
+    from logs
+)
+select distinct(num) as ConsecutiveNums
+from x
+where num = pre1 and num = pre2
