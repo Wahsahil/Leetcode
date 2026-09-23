144bc2edb2ef2f056067b9da869c49ad4b91466f
Sahil Chaurasia <139702894+Wahsahil@users.noreply.github.com>
2026-09-23 15:38:35 +0530

    Time: 1002 ms (44.65%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+WITH x AS (
+    SELECT person_name,
+           turn,
+           SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+)
+SELECT person_name
+FROM x
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
