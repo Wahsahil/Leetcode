c9b5b6292549620153dccc17917be8f32193ff3b
Sahil Chaurasia <139702894+Wahsahil@users.noreply.github.com>
2026-09-24 19:41:56 +0530

    Time: 266 ms (49.17%), Space: 17.3 MB (7.5%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -3,7 +3,6 @@ class Solution(object):
         d = {}
         i = 0
         m = 0
-        cf = 0
         for j in range(len(fruits)):
             d[fruits[j]] = d.get(fruits[j], 0) + 1
             while len(d) > 2:
@@ -11,6 +10,5 @@ class Solution(object):
                 if d[fruits[i]] == 0:
                     del d[fruits[i]]
                 i += 1
-            cf = j - i + 1
-            m = max(cf, m)
+            m = max(j - i + 1, m)
         return m
